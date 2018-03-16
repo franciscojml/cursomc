e8241f0041bf8de9c4f1bc3e0060114a9dd77cae
Francisco Lima <franciscojml@gmail.com>
2018-03-16 16:18:00 -0300

    Dominio categoria

--- src/main/java/com/fjlima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fjlima/cursomc/resources/CategoriaResource.java
@@ -1,19 +1,28 @@
 package com.fjlima.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fjlima.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(new Integer(1), "Informática");
+		Categoria c2 = new Categoria(new Integer(2), "Escritório");
+
+		List<Categoria> l = new ArrayList<>();
+		l.add(c1);
+		l.add(c2);
+
+		return l;
 	}
 
-	
-	
-	
 }

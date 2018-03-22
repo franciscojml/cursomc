b82d8e496ccda727e55980ac517a3ec3ecb33e25
Francisco Lima <franciscojml@gmail.com>
2018-03-22 14:55:31 -0300

    Criando operação de instanciação

--- src/main/java/com/fjlima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fjlima/cursomc/resources/CategoriaResource.java
@@ -1,28 +1,26 @@
 package com.fjlima.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fjlima.cursomc.domain.Categoria;
+import com.fjlima.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria c1 = new Categoria(new Integer(1), "Informática");
-		Categoria c2 = new Categoria(new Integer(2), "Escritório");
+	@Autowired
+	private CategoriaService service;
 
-		List<Categoria> l = new ArrayList<>();
-		l.add(c1);
-		l.add(c2);
-
-		return l;
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria categoria = service.find(id);
+		return ResponseEntity.ok().body(categoria);
 	}
 
 }

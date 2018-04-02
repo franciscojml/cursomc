290af3aa9bf5183231585d35036ffbbc631234c7
Francisco Lima <franciscojml@gmail.com>
2018-04-02 16:59:33 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/fjlima/cursomc/config/DevConfig.java
+++ src/main/java/com/fjlima/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.fjlima.cursomc.services.DBService;
+import com.fjlima.cursomc.services.EmailService;
+import com.fjlima.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,6 +30,11 @@ public class DevConfig {
 
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() throws ParseException {
+		return new SmtpEmailService();
 	}
 
 }
